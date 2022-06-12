e8b71f2c9b34f8d94cfff47453255927eda10230
Gauri Joshi <gauri728@gmail.com>
2022-06-12 14:59:59 -0700

    Test - Shift class initialize

---
+++ lib/shift.rb
@@ -0,0 +1,6 @@
+class Shift
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,4 +4,4 @@ SimpleCov.start
 require_relative "enigma_spec"
 require_relative "key_generator_spec"
 require_relative "offset_spec"
-require_relative "shift"
+require_relative "shift_spec"
