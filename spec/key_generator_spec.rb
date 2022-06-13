3403883c58bf9517332a2b0e4c0f93d942876c9c
Gauri Joshi <gauri728@gmail.com>
2022-06-13 15:01:26 -0700

    Create KeyGenerator Module

--- spec/key_generator_spec.rb
+++ spec/key_generator_spec.rb
@@ -5,23 +5,23 @@ require './lib/key_generator'
 RSpec.describe KeyGenerator do
   before :each do
     @enigma = Enigma.new
-    @key_generator = KeyGenerator.new
+    # @key_generator = KeyGenerator.new
   end
 
-  it "exists" do
-    expect(@key_generator).to be_a(KeyGenerator)
-  end
+  # it "exists" do
+  #   expect(@key_generator).to be_a(KeyGenerator)
+  # end
 
   it "assigns a random 5 digit number as a string to Enigma" do
     expect(@enigma.key_generator).to be_a(String)
   end
 
   it "can convert the 5 digit string into an array" do
-    expect(@key_generator.string_to_array).to be_a(Array)
+    expect(@enigma.string_to_array).to be_a(Array)
   end
 
   it "can parse through an array to create consecutive pairs of digits" do
-    expect(@key_generator.pairs).to be_a(Array)
-    expect(@key_generator.pairs.length).to eq(4)
+    expect(@enigma.pairs).to be_a(Array)
+    expect(@enigma.pairs.length).to eq(4)
   end
 end
