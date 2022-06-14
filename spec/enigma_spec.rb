bbd8b1a1acc8db4f0ae48f9c24fab9d16818f18d
Gauri Joshi <gauri728@gmail.com>
2022-06-14 12:11:36 -0700

    Includes module tests inside Enigma spec

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -21,6 +21,18 @@ RSpec.describe Enigma do
     expect(@enigma.message_split("hello world")).to eq(["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"])
   end
 
+  it "can generate a random 5 digit key in the form of a string" do
+    expect(@enigma.key_generator).to be_a(String)
+  end
+
+  it "can convert the 5 digit key into an array" do
+    expect(@enigma.string_to_array).to be_a(Array)
+  end
+
+  it "can iterate through the 5 digit key as an array and convert them into consecutive pairs that are joined into one integer" do
+    expect(@enigma.pairs).to be_a(Integer)
+  end
+
   it "can encrypt a message with a key and date" do
     expect(@enigma.encrypt("hello world", "02715", "040895")).to eq({
                                           encryption: "keder ohulw",
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,5 @@ require 'simplecov'
 SimpleCov.start
 
 require_relative "enigma_spec"
-# require_relative "key_generator_spec"
 require_relative "offset_spec"
 require_relative "shift_spec"
